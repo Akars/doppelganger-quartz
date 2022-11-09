cdd65c121009f7340f568ae20eac43d425200cc8
Akars <williamli973@gmail.com>
2022-11-09 16:15:16 +0100

    Calculator done and Discount started v1

--- js/tests/calculotor.test.js
+++ js/tests/calculotor.test.js
@@ -1,7 +1,14 @@
 const test = require('tape')
+const Calculator = require('../calculator')
+
+
 
 test('should throw when not authorized', (t) => {
-  // TODO: write a test that fails due to the bug in
-  // Calculator.divide()
+  const falseAuthorizer = { authorize: (() => false) }
+  const calculator = new Calculator(falseAuthorizer)
+
+  calculator.divide()
+  // We verify if the call of divide() was authorize
+  t.equal(calculator.authorizer.authorize(), true)
   t.end()
 })
--- js/tests/discount-applier.test.js
+++ js/tests/discount-applier.test.js
@@ -1,8 +1,18 @@
 const test = require('tape')
+const DiscountApplier = require('../discount-applier')
+
+const discountApplier = new DiscountApplier({
+  notificationSent: 0,
+  notify: ((user, message) => this.notificationSent++)
+})
+
+const testUsers = ['Benoit', 'Titouan', 'William']
 
 test('apply v1', (t) => {
   // TODO: write a test that fails due to the bug in
-  // DiscountApplier.applyV1
+  const discount = '3$'
+  discountApplier.applyV1(discount, testUsers)
+  t.equal(discountApplier.notifier.notificationSent, testUsers.length)
   t.end()
 })
 
