9d8fecc23f8616ba971be2771f46780a485e642d
Akars <williamli973@gmail.com>
2022-11-14 21:21:48 +0100

    java - CalculatorTest

--- java/src/test/java/fr/efrei/CalculatorTest.java
+++ java/src/test/java/fr/efrei/CalculatorTest.java
@@ -1,10 +1,15 @@
 package fr.efrei;
 
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
   @Test
   void should_throw_when_not_authorized() {
     // TODO: trigger the bug in Calculator.divide() by implementing the Authorizer interface
+    Authorizer notAuthorized = () -> false;
+    Calculator calculator = new Calculator(notAuthorized);
+
+    assertThrows(UnauthorizedAccessException.class, () -> calculator.divide(1, 2));
   }
 }
